5f3a0b4bcc7c6bea2ed7fadb0eb67c5a08d58f7b
unknown <J0127@JRY.COM>
2017-12-28 20:33:17 +0800

    add js2-mode and add test.js
    add js2-mode and add test.js

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -16,8 +16,13 @@
 (defvar jimmy/packages '(
 			 company
 			 monokai-theme
-			 
+			 hungry-delete
+			 swiper
+			 counsel
+			 smartparens
+			 js2-mode
 			 ) "Default packages")
+(setq package-selected-packages jimmy/packages)
 
 (defun jimmy/packages-installed-p ()
   (loop for pkg in jimmy/packages
@@ -30,6 +35,34 @@
   (dolist (pkg jimmy/packages)
     (when (not (package-installed-p pkg))
       (package-install pkg))))
+
+(require 'hungry-delete)
+(global-hungry-delete-mode)
+
+;;swiper config
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+(setq enable-recursive-minibuffers t)
+(global-set-key "\C-s" 'swiper)
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "C-h f") 'counsel-describe-function)
+(global-set-key (kbd "C-h v") 'counsel-describe-variable)
+
+;;smartparens config
+(require 'smartparens-config)
+;;局部使用
+;;(add-hook 'emacs-lisp-mode-hook 'smartparens-mode)
+;;全局使用
+(smartparens-global-mode t)
+
+;;config js2-mode for js files
+(setq auto-mode-alist
+      (append
+       '(("\\.js\\'" . js2-mode))
+       auto-mode-alist))
+       
 
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
@@ -47,8 +80,10 @@
 (recentf-mode 1)
 (setq recentf-max-menu-items 25)
 
+
+;;自定义按键
 (global-set-key (kbd "<f2>") 'open-my-init-file)
-;;(global-set-key "\C-x\ \C-r" 'recentf-open-files)
+(global-set-key "\C-x\ \C-r" 'recentf-open-files)
 
 (global-hl-line-mode t)
 (load-theme 'monokai t)
@@ -62,7 +97,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (monokai-theme company))))
+ '(company-idle-delay 0.08)
+ '(company-minimum-prefix-length 1))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
